a8c072f005b3e161167f62bea0cfb39c7511dd37
ppreshant <ppresh.ant@gmail.com>
2019-04-30 12:25:02 -0500

    downloaded missing files from server ruf.rice.edu

---
+++ documents/jwemanuel.php
@@ -0,0 +1,12 @@
+<html>
+ <head>
+  <title>Resume</title>
+ </head>
+ <body>
+ <center>
+Jerry Emanuel
+1301 Richmond Ave. #Y6, Houston, TX, 77006 (585)298-4333  jwemanuel@gmail.com
+</center>
+
+ </body>
+</html>
